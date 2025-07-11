97ebe647884ca065da07e849097630ab5cfabb14
LandielDurans <129320343+LandielDurans@users.noreply.github.com>
2025-07-11 12:03:05 -0300

    feat: add http file and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { CreateRoom } from "./pages/Create-room";
-import { Room } from "./pages/Room";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { Room } from './pages/room'
 
 const queryClient = new QueryClient()
 
